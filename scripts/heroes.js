2957f66b6bf882a08759f68c889423f0d2052b9b
LeandroPura08 <thanoy08@gmail.com>
2023-11-01 22:47:26 +0800

    added some image and js

---
+++ scripts/heroes.js
@@ -0,0 +1,84 @@
+const supports = [
+  "/images/supports/Chen.jpg",
+  "/images/supports/crystal-maiden.jpg",
+  "/images/supports/earth-spirit.jpg",
+  "/images/supports/earthshaker.jpg",
+  "/images/supports/witch-doctor.jpg",
+  "/images/supports/lion.jpg"
+];
+
+
+const offlanes = [
+  "/images/offlane/balanar.jpg",
+  "/images/offlane/centour.jpg",
+  "/images/offlane/DK.jpg",
+  "/images/offlane/legion.jpg",
+  "/images/offlane/tidehunter.jpg",
+  "/images/offlane/timbersaw.jpg"
+];
+
+const safelanes = [
+  "/images/safelane/drow.jpg",
+  "/images/safelane/huskar.jpg",
+  "/images/safelane/Medusa.jpg",
+  "/images/safelane/Snipe.jpg",
+  "/images/safelane/sven.jpg",
+  "/images/safelane/terrorblade.jpg"
+];
+
+let  output ='';
+offlanes.forEach((hero) =>{
+  output = output +
+  `
+  <div class="image">
+    <img class="pic" src=${hero}>
+  </div>
+  `
+  document.querySelector('.image-container').innerHTML = output
+});
+
+document.querySelectorAll('.options').forEach((options)=>{
+  options.addEventListener('click',()=>{
+  document.querySelector('.selected').classList.remove("selected");
+  const role = options.dataset.role;
+
+  if(role =="offlanes"){
+    output = '';
+    document.querySelector('.offlanes').classList.add("selected");
+    offlanes.forEach((hero) =>{
+      output = output +
+      `
+      <div class="image">
+        <img class="pic" src=${hero}>
+      </div>
+      `
+      document.querySelector('.image-container').innerHTML = output
+    });
+
+  }else if(role =="safelanes"){
+    output = '';
+    document.querySelector('.safelanes').classList.add("selected");
+    safelanes.forEach((hero) =>{
+      output = output +
+      `
+      <div class="image">
+        <img class="pic" src=${hero}>
+      </div>
+      `
+      document.querySelector('.image-container').innerHTML = output
+    });
+  }else if(role =="supports"){
+    output = '';
+    document.querySelector('.supports').classList.add("selected");
+    supports.forEach((hero) =>{
+      output = output +
+      `
+      <div class="image">
+        <img class="pic" src=${hero}>
+      </div>
+      `
+      document.querySelector('.image-container').innerHTML = output
+    });
+  }
+  })
+})
